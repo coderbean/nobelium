bfcee74e6e7376c3137e5e6f4439ba9a148279ca
coderbean <haozhangbo@hotmail.com>
2025-03-13 15:30:38 +0000

    Update blog description in configuration

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Cyber.Z',
   email: 'jakebobo.zb@gmail.com',
   link: 'https://github.com/coderbean',
-  description: '',
+  description: 'Cyber.Z的个人博客',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
