61b770a0bb5c438bb418d8c5717e71b7c4c0a579
JacobChang <haozhangbo123@gmail.com>
2025-03-13 21:45:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: '赛博笔记',
+  author: 'Cyber.Z',
+  email: 'haozhangbo123@gmail.com',
+  link: 'https://github.com/coderbean',
+  description: '随便写点东西',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
