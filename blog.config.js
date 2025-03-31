027afd3cc5e000cac1fc658fa5afbd7906471ddf
zhangbo.jacob <zhangbo.jacob@bytedance.com>
2025-03-31 11:47:03 +0800

    Update author name and blog description in configuration
    Change-Id: I972124cddbc040cb21fcb70348d1151dd6966ee6

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
   title: '赛博笔记',
-  author: 'Cyber.Z',
+  author: 'Cyburger',
   email: 'jakebobo.zb@gmail.com',
   link: 'https://github.com/coderbean',
-  description: 'Cyber.Z的个人博客',
+  description: 'Cyburger\'s Note',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
